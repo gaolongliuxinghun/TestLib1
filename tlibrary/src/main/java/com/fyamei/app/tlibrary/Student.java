26af7dd76ecd2821b9db3231b52f421723bacf0d
gaolong <13714117447@163.com>
2019-10-31 09:28:15 +0800

    Initial commit

---
+++ tlibrary/src/main/java/com/fyamei/app/tlibrary/Student.java
@@ -0,0 +1,20 @@
+package com.fyamei.app.tlibrary;
+
+/**
+ * Created by gl on 2019/10/30.
+ */
+
+public class Student {
+    public String name;
+    public int age;
+
+    public Student() {
+    }
+
+    public Student(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+
+}
---
+++ tlibrary/src/main/java/com/fyamei/app/tlibrary/T.java
@@ -0,0 +1,18 @@
+package com.fyamei.app.tlibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+public class T {
+
+    public static void show(Context context,String string){
+        Student student = new Student(string,3);
+        Gson gson = new GsonBuilder().create();
+        String s = gson.toJson(student);
+
+        Toast.makeText(context, s, Toast.LENGTH_SHORT).show();
+    }
+}
